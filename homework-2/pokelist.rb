e546b790546a029d73fc9242a8e296dfa80d2ff8
AlexeyRudenkov <108358127+alex1-11@users.noreply.github.com>
2022-12-01 23:34:37 +0300

    To pokelist added the add_poke method for creating a single pokemon

--- homework-2/pokelist.rb
+++ homework-2/pokelist.rb
@@ -5,6 +5,33 @@ def getpoke_qty
     if qty.class == Integer && qty > 0
       return qty
     end
-    puts "Error! Number must be a positive integer."
+    puts 'Error! Number must be a positive integer.'
   end
+end
+
+def add_poke
+  poke = {}
+  puts 'Adding new pokemon...'
+  loop do
+    print 'Name > '
+    name = gets.chomp
+    if name.empty?
+      puts 'Error! Name can`t be blank!'
+    else
+      poke[:name] = name
+      break
+    end
+  end
+  loop do
+    print 'Color > '
+    color = gets.chomp
+    if color.empty?
+      puts 'Error! Color can`t be blank! Write "transparent" ' +
+           'if pokemon doesn`t have any color at all :)'
+    else
+      poke[:color] = color
+      break
+    end
+  end
+  poke
 end
