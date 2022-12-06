baa925035adc7829de8cb58a88c281bab0bb4801
AlexeyRudenkov <108358127+alex1-11@users.noreply.github.com>
2022-12-06 22:54:24 +0300

    Made task2, but need to fix the end condition

--- homework-3/2_students.rb
+++ homework-3/2_students.rb
@@ -1,16 +1,32 @@
-FILENAME = 'students.txt'
+FILENAME = 'students.txt'.freeze
+RESULTS = 'results.txt'.freeze
 
 def choose_students
   # Read file
   data = File.readlines(FILENAME).map(&:chomp)
-  # Ask for age input
-  asked_age = gets.to_i
-  # Find exact age match
-  def pick_by_age(age)
+  # Loop through students' list
+  loop do
+    # Ask for age input
+    print 'Enter student`s age to search for: '
+    asked_age = gets.chomp
+    # Finish early if user input exit code '-1'
+    break if asked_age == '-1'
+    # Find exact age match
+    data.each do |line|
+      cells = line.split
+      if cells[2] == asked_age
+        # Write (append) found student to file results.txt
+        File.write(RESULTS, "#{line}\n", mode: "a")
+        # Erase the line value (don't delete element during loop)
+        line = nil
+      end
+    end
+    # Get rid of erased lines
+    data.compact!
+    # Finish if all the students were written to results.txt
+    break if data.empty?
+    # Loop back for age input otherwise
   end
-  # Write (append) found student to file results.txt
-  # Loop back for age input
-  # Add end conditions
-  # If all students were written to results.txt
-  # If user input is -1
 end
+
+choose_students
