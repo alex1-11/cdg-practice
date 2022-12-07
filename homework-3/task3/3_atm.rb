35865118db4a4ca0f6b320c3009b286b84f91773
AlexeyRudenkov <108358127+alex1-11@users.noreply.github.com>
2022-12-08 01:32:57 +0300

    Completed task3. Still needs debugging

--- homework-3/task3/3_atm.rb
+++ homework-3/task3/3_atm.rb
@@ -12,20 +12,23 @@ TIP = %(  B - show current account balance,
 
 Enter the command: ).freeze
 
-COMMANDS = ['B', 'D', 'W', 'Q']
+COMMANDS = ['B', 'D', 'W', 'Q'].map(&:freeze)
 
 def ask_next
-  puts 'Do you want anything else? Type in one of the options:'.freeze, TIP
+  puts 'Do you want anything else to do? Type in one of the options:'.freeze, TIP
 end
 
-# Verify if input is Float type -- https://stackoverflow.com/a/31411319/20260711
+# Verify if input is positive and Float type -- https://stackoverflow.com/a/31411319/20260711
 def input_float
-  begin
-    amount = Float(gets.chomp)
-  rescue ArgumentError
-    puts 'Amount should be a NUMBER. Floating point is allowed.'.freeze
-    retry
+  amount = Float(gets.chomp)
+  if amount <= 0
+    puts 'Amount should be a POSITIVE number!'
+    amount = 0
   end
+  amount
+rescue ArgumentError
+  puts 'Amount should be a NUMBER! Floating point is allowed.'
+  0
 end
 
 # Puts user's account balance at console
@@ -36,22 +39,28 @@ end
 # Asks for amount and increses the account balance
 def deposit(account)
   print 'Enter amount you wish to deposit: '
-  amount = 0.0
-  # Verify user input: must be positive and float/int
-  until amount.positive?
-    amount = input_float
-    puts 'Amount should be a POSITIVE number' if amount <= 0
-  end
+  amount = input_float
+  puts 'Deposit successfully completed.' if amount != 0
   account + amount
 end
 
-
+# Asks for amount to withdraw, checks if it's possible and updates account
 def withdraw(account)
-  
+  print 'Enter amount you wish to withdraw: '
+  amount = input_float
+  # Make sure user has sufficient balance
+  if amount > account
+    puts 'Balance insufficient!'
+    amount = 0
+  end
+  puts 'Withdrawal successfully completed.' if amount != 0
+  account - amount
 end
 
+# Saves current account balance to the file and closes it
 def quit(account)
-  
+  puts 'Closing session...'
+  File.write(BALANCE, account)
 end
 
 # Main script, starts ATM with full functionality
@@ -60,7 +69,7 @@ def use_atm
   account = File.exist? BALANCE ? File.read(BALANCE).to_f : DEFAULT_BALANCE
   # Interact with user, give instructions
   puts GREETING, TIP
-  # Ask user input, force to upcase 
+  # Ask user input, force to upcase
   loop do
     command = gets.chomp.upcase
     # Update account balance varibale depending on command -- https://www.alanwsmith.com/posts/assigning-ruby-variables-with-a-case-statement--20en0nhdumt0
@@ -69,15 +78,19 @@ def use_atm
                 balance(account)
               when 'D'
                 deposit(account)
+                balance
               when 'W'
                 withdraw(account)
+                balance
               when 'Q'
                 quit(account)
+                break
               else
                 puts "Input error. No such command. Type one of these chars:\n", COMMANDS
               end
     ask_next
   end
+  puts 'Thanks for using Royal bank of Stormwind! Good bye!'
 end
 
 # use_atm
