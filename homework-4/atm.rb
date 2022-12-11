5ad587a3678074753b98dd708d40bfb8413ef2b1
AlexeyRudenkov <108358127+alex1-11@users.noreply.github.com>
2022-12-11 20:42:56 +0300

    Moved input_float method inside Atm class. Made some style fixes

--- homework-4/atm.rb
+++ homework-4/atm.rb
@@ -14,26 +14,11 @@ TIP = %(  To proceed type-in one of the options:
 
 Enter the command: ).freeze
 
-COMMANDS = ['B', 'D', 'W', 'Q'].map(&:freeze)
-
-# Verify if input is positive and Float type -- https://stackoverflow.com/a/31411319/20260711
-def input_float
-  amount = Float(gets.chomp)
-  puts
-  if amount <= 0
-    puts 'ERROR! Amount should be a POSITIVE number! <<<<<<<<<<<<<<<<<'
-    amount = 0
-  end
-  amount
-rescue ArgumentError
-  puts 'ERROR! Amount should be a NUMBER! Floating point is allowed. <<<<<<<<<<<<<<<<<'
-  0
-end
-
+# The ATM app. Initialize an instance as a variable.
+# Then initialize a CommandController for interaction.
 class Atm
-  # Starts ATM with full functionality
+  # Starts ATM, loads up balance or uses default size if not defined
   def initialize
-    # Load up balance or use default size if not defined
     @account = File.exist?(BALANCE) ? File.read(BALANCE).to_f : DEFAULT_BALANCE
   end
 
@@ -45,12 +30,31 @@ class Atm
          '__________________________________________'
   end
 
-  # Asks for amount and increses the account balance
+  # Gets correct float from user input
+  def input_float
+    amount = Float(gets.chomp)
+    puts
+
+    # Verify if input is positive
+    if amount <= 0
+      puts 'ERROR! Amount should be a POSITIVE number! <<<<<<<<<<<<<<<<<'
+      amount = 0
+    end
+    amount
+
+  # Handles error and puts it to console if input is not Float type -- https://stackoverflow.com/a/31411319/20260711
+  rescue ArgumentError
+    puts 'ERROR! Amount should be a NUMBER! Floating point is allowed. <<<<<<<<<<<<<<<<<'
+    # Returns zero if input was invalid type
+    0
+  end
+
+  # Asks for amount to deposit and increases the account balance
   def deposit
     print 'Enter amount you wish to deposit: '
     amount = input_float
     puts ''
-    puts 'Deposit successfully completed.' if amount > 0
+    puts 'Deposit successfully completed.' if amount.positive?
     @account += amount
   end
 
@@ -59,11 +63,13 @@ class Atm
     print 'Enter amount you wish to withdraw: '
     amount = input_float
     puts ''
+
     # Make sure user has sufficient balance
     if amount > @account
       puts 'ERROR! Balance insufficient! <<<<<<<<<<<<<<<<<'
       amount = 0
     end
+
     puts 'Withdrawal successfully completed.' if amount != 0
     @account -= amount
   end
@@ -78,25 +84,32 @@ class Atm
          'Thanks for using Royal bank of Stormwind! Good bye!',
          '==================================================='
   end
+
+  private :input_float
 end
 
 # Asks user for command input and triggers the action ATM should take
 class CommandController
   def initialize(atm)
     # Interact with user, give starting instructions
-    puts GREETING, TIP
+    puts GREETING
+
     loop do
       # Ask user input, force to upcase
+      puts TIP
       command = gets.chomp.upcase
       puts ''
-      # Update account balance varibale depending on command -- https://www.alanwsmith.com/posts/assigning-ruby-variables-with-a-case-statement--20en0nhdumt0
+
+      # Execute the given command depending on preset
       case command
       when 'B'
         atm.balance
       when 'D'
         atm.deposit
+        atm.balance
       when 'W'
         atm.withdraw
+        atm.balance
       when 'Q'
         atm.quit
         break
