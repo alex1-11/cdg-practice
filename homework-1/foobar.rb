145df23895020b77a91e037ada6b26387abea111
AlexeyRudenkov <108358127+alex1-11@users.noreply.github.com>
2022-12-11 13:25:23 +0300

    Corrected styling mistakes

--- homework-1/foobar.rb
+++ homework-1/foobar.rb
@@ -8,8 +8,8 @@ def foobar(num1, num2)
   end
 end
 
-print "Enter num1: "
+print 'Enter num1: '
 input1 = gets.to_i
-print "Enter num2: "
+print 'Enter num2: '
 input2 = gets.to_i
 puts "foobar(#{input1}, #{input2}) returns #{foobar(input1, input2)}"
