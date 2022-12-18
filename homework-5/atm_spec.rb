cbcf868281947f65e7b1df073fb18da32bba4762
AlexeyRudenkov <108358127+alex1-11@users.noreply.github.com>
2022-12-19 00:24:15 +0300

    Trying to make test for atm. Stuck on command loop

--- homework-5/atm_spec.rb
+++ homework-5/atm_spec.rb
@@ -1,20 +1,26 @@
 require 'rspec'
 require './atm'
 
-RSpec.describe 'atm.rb'
-before { atm = Atm.new }
-
-describe '.input_float' do
-  it 'asks user to input float' do
-    expect(atm.input_float).to eq Float
+RSpec.describe 'atm.rb' do
+  subject { Atm.new }
+  describe '#initialize' do
+    before { allow_any_instance_of(Kernel).to recieve(:gets).and_return('q') }
+    it { is_expected.to be positive }
   end
 
-  context 'when user input in not float or int' do
-    let(:input) { 'float' }
+  # describe '.input_float' do
+  #   it 'asks user to input float' do
+  #     expect(atm.input_float).to eq Float
+  #   end
 
-    it { expect { subject }.to raise_error(ArgumentError) }
-  end
+  #   context 'when user input in not float or int' do
+  #     let(:input) { 'float' }
 
-  desribe '#deposit' do
-  end
+  #     it { expect { subject }.to raise_error(ArgumentError) }
+  #   end
+
+
+  #   desribe '#deposit' do
+  #   end
+  # end
 end
