d9194c08abdd854d9d5ed4ee01a4912cb71bba56
AlexeyRudenkov <108358127+alex1-11@users.noreply.github.com>
2023-01-05 02:29:38 +0300

    Made primitive `template_wrapper` method in `app.rb`, used it in routing. Moved `index.html` to `/static`, added `template.html` for basic common style.

--- homework-6/app.rb
+++ homework-6/app.rb
@@ -16,26 +16,22 @@ class App
 
     case req.path
     when '/'
-      [200, { 'Content-Type' => 'text/html' }, File.readlines('./index.html')]
+      [200, { 'Content-Type' => 'text/html' }, File.readlines('static/index.html')]
     when '/balance'
       params['action'] = 'B'
       result = atm_controller(params)
-      [200, { 'Content-Type' => 'text/html' }, ["<h3>#{result}</h3>",
-                                                "<br><a href='/'>Home</a>"]]
+      [200, { 'Content-Type' => 'text/html' }, template_wrapper(result)]
     when '/deposit'
       params['action'] = 'D'
       result = atm_controller(params)
-      [200, { 'Content-Type' => 'text/html' }, ["<h3>#{result}</h3>",
-                                                "<br><a href='/'>Home</a>"]]
+      [200, { 'Content-Type' => 'text/html' }, template_wrapper(result)]
     when '/withdraw'
       params['action'] = 'W'
       result = atm_controller(params)
-      [200, { 'Content-Type' => 'text/html' }, ["<h3>#{result}</h3>",
-                                                "<br><a href='/'>Home</a>"]]
+      [200, { 'Content-Type' => 'text/html' }, template_wrapper(result)]
     else
-      [404, { 'Content-Type' => 'text/html' }, ['<h1>404</h1>',
-                                                '<p>Page not found. Try to check your request.</p>',
-                                                "<br><a href='/'>Home</a>"]]
+      [404, { 'Content-Type' => 'text/html' },
+       template_wrapper('<h1>404</h1><p>Page not found. Try to check your request.</p>')]
     end
   end
 
@@ -51,5 +47,12 @@ class App
       "Previous balance was <u>#{atm.balance}</u> gold.<br>"\
       "New balance after withdraw attempt is <u>#{atm.withdraw(params['value'])}</u> gold."
     end
+  end
+
+  # Wraps results into basic stylized html template
+  def template_wrapper(content)
+    html = File.readlines('static/template.html')
+    html << ["<h3>#{content}</h3>", '    </div>', '  </body>', '</html>']
+    html.flatten
   end
 end
